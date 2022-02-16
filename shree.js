165aba915247389443514761589a80a06c1757ca
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-02-16 11:52:25 +0530

    dark mode

---
+++ shree.js
@@ -0,0 +1,16 @@
+function shyam(){
+	x=document.getElementById("ve")
+	y=document.getElementById("bl")
+	if(x==x){
+		x.style="background-color:white;color:black"
+		y.style="background-color:black;color:white"
+	}	
+	else{
+		alert("thanks")
+	}
+}
+function ram(){
+	x.style="background-color:black;color:white"
+		y.style="background-color:white;color:black"
+
+}
